bd99d3afe68d0466fc266f68bb61c168191c7f57
Swapnil-gautam <swapnilgtm1998@gmail.com>
2025-02-06 17:11:13 -0500

    all first

---
+++ crawl4AI-examples/1-crawl_single_page.py
@@ -0,0 +1,13 @@
+import asyncio
+from crawl4ai import *
+
+async def main():
+    async with AsyncWebCrawler() as crawler:
+        result = await crawler.arun(
+            # url="https://ai.pydantic.dev/",
+            url="https://swapnil-gautam.github.io/Portfolio_Website/",
+        )
+        print(result.markdown)
+
+if __name__ == "__main__":
+    asyncio.run(main())
